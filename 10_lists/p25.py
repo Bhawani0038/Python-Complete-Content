d91c21af8acdc037bc6bfd285747e2f8e6d3c236
Bhawani Singh <bhawanisingh0038@gmail.com>
2026-09-18 11:33:41 +0530

    healthcare prject added

--- 10_lists/p25.py
+++ 10_lists/p25.py
@@ -1,3 +1,10 @@
-words = ["Python", "is", "fun"]
-sentence = " ".join(words)
-print(sentence)
+# words = ["Python", "is", "fun"]
+# sentence = " ".join(words)
+# print(sentence)
+
+def display_info(name, price, description):
+    print("name =", name)
+    print("price = ", price)
+    print("description= ", description)
+    
+
--- 10_lists/p26.py
+++ 10_lists/p26.py
@@ -1,3 +1,4 @@
 numbers = [1, 2, 3]
 text = ", ".join(str(number) for number in numbers)
 print(text)
+
